35cb945b1168ee73fcddb05d778a9dcf8c213df1
Thara Supasiti <sapiensis@gmail.com>
2021-08-25 09:35:53 +1000

    add sign up page

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,22 @@
 const btnLogin = document.querySelector('#btn-login');
+const btnSignUp = document.querySelector('#btn-signup');
 
 const goToLoginHandler = async (event) => {
   event.preventDefault();
-
   document.location.replace('/login');
 }
 
-btnLogin.addEventListener('click', goToLoginHandler);
+const goToSignUpHandler = async (event) => {
+  event.preventDefault();
+  console.log('sign up button clicked')
+  document.location.replace('/signup');
+}
+
+
+if (btnLogin){
+  btnLogin.addEventListener('click', goToLoginHandler);
+}
+
+if (btnSignUp){
+  btnSignUp.addEventListener('click', goToSignUpHandler);
+}
