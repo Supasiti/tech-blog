d3973e94ad9cac89ff0dee92a6d73e68bf6b4c1e
Thara Supasiti <sapiensis@gmail.com>
2021-08-26 09:15:58 +1000

    add delete post UI

--- public/js/updatePost.js
+++ public/js/updatePost.js
@@ -1,16 +1,24 @@
 const editPostForm = document.querySelector('#edit-post-form');
+const deletePostBtn = document.querySelector('#delete-post-btn');
 
+// get post id from href
+// expect posts/:id/edit
+// return - int
+const getPostId = () => {
+  const path = document.location.href.split('?')[0]
+  const routes = path.split('/');
+  const result = routes[routes.length - 2]; 
+  return result;
+}
+
+// update post
 const updatePostHandler = async (event) => {
   event.preventDefault();
 
   // get inputs
   const title = document.querySelector('#post-title-input').value.trim();
   const content = document.querySelector('#post-content-textarea').value.trim();
-  
-  // expect posts/:id/edit
-  const path = document.location.href.split('?')[0]
-  const routes = path.split('/');
-  const postId = routes[routes.length - 2]; 
+  const postId = getPostId(); 
 
   // update database
   const response = await fetch(`/api/posts/${postId}`, {
@@ -27,5 +35,26 @@ const updatePostHandler = async (event) => {
   }
 }
 
+// delete post
+const deletePostHandler = async (event) => {
+  event.preventDefault();
+
+  const postId = getPostId();
+  
+  // delete database
+  const response = await fetch(`/api/posts/${postId}`, {
+    method: 'DELETE',
+  });
+
+  // rerender the page
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete post');
+  }
+}
+
+
 addFormValidation(editPostForm);
 editPostForm.addEventListener('submit', updatePostHandler);
+deletePostBtn.addEventListener('click', deletePostHandler);
